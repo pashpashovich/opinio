8566c9b79c31ec4956988540759ad5254942147e
pashpashovich <pavel.kosovich2004@gmail.com>
2024-11-17 16:38:37 +0300

    Init

---
+++ src/main/java/by/opinio/OpinioApplication.java
@@ -0,0 +1,12 @@
+package by.opinio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpinioApplication.class, args);
+
+    }
+}
---
+++ src/test/java/by/opinio/OpinioApplicationTests.java
@@ -0,0 +1,13 @@
+package by.opinio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
